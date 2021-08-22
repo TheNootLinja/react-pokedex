0c0112cf5bb20a487ef0f2ba9be1951798ba7a6e
TheNootLinja <thenootlinja@gmail.com>
2021-08-22 02:19:02 -0400

    adjust styling of header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   const [pokeData, setPokeData] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
 
+  const handleClick = (name) => {
+    setSearchTerm(name);
+    console.log(searchTerm);
+  }
+
   const fetchPokemon = () => {
     fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=898')
     .then((response) => response.json())
@@ -28,7 +33,7 @@ function App() {
     {pokeData&&pokeData.results.map((pokemon) => {
     num = num + 1
       return (
-          <Card name={pokemon.name} num={num} key={pokemon.name}/>
+          <Card clickFunc={handleClick} name={pokemon.name} num={num} key={pokemon.name}/>
           // <div>
           //   <p>{pokemon.name}</p>
           //   <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`} alt="" />

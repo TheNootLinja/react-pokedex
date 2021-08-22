1515816ab73a9510176ef2ef362f66d702703085
TheNootLinja <thenootlinja@gmail.com>
2021-08-22 02:18:33 -0400

    Adjust styling of card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,8 @@
 import "./Card.styles.css"
 
-function Card({name, num}) {
+function Card({name, num, clickFunc}) {
     return (
-      <div className="card">
+      <div className="card" onClick={() => clickFunc(name)} data={name}>
         <p className="card__name">{name}</p>
         <img className="card__img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`} alt="" />
       </div>
